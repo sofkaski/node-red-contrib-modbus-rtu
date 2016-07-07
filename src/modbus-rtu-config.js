ba7ce90ced8c9f65f17a00731798e300ffe46f07
Jyrki K <jyrkiokaski@gmail.com>
2016-07-07 20:25:00 +0300

    Modbus reading works. Intermedite commit

--- src/modbus-rtu-config.js
+++ src/modbus-rtu-config.js
@@ -2,50 +2,61 @@ module.exports = function(RED) {
     function ModbusRTUConfigNode(config) {
         var SerialPort = require('serialport');
         var modbus = require('modbus-rtu');
-        var log = require('tracer').console();
+//        var log = require('tracer').console();
+        var vsprintf = require("sprintf-js").vsprintf;
+        var log = RED.log;
+
 
         RED.nodes.createNode(this,config);
-        this.name = config.name;
-        this.dataType = config.dataType;
-        this.unit_id = parseInt(config.unit_id);
-        this.serial_device = config.serial_device;
-        this.serial_speed = parseInt(config.serial_speed);
-        this.serial_parity = config.serial_parity;
-        this.serial_databits = parseInt(config.serial_databits);
-        this.serial_stopbits = parseInt(config.serial_stopbits);
+        var node = this;
+        node.name = config.name;
+        node.dataType = config.dataType;
+        node.unit_id = parseInt(config.unit_id);
+        node.serial_device = config.serial_device;
+        node.serial_speed = parseInt(config.serial_speed);
+        node.serial_parity = config.serial_parity;
+        node.serial_databits = parseInt(config.serial_databits);
+        node.serial_stopbits = parseInt(config.serial_stopbits);
 
-        this.on('input', function(msg) {
+        node.on('input', function(msg) {
             
         });
 
-        var node = this;
         node.modbusMaster = null;
+        node.serialPort = null;
+        log.info(vsprintf("Defined parameters for device %s (%s baud)", [node.serial_device, node.serial_speed]));
         
         // connection initialization. Create serial device and then modbus master on top of that
         node.initializeRTUConnection = function(callback) {
-            var serialPort = new SerialPort(this.serial_device, {
-                baudrate: this.serial_speed});
-            if (serialPort) {
-                // handle serial port open
-                serialPort.on('open', function() {
-                    log.info("Created serial port");
-                    node.modbusMaster = new modbus.Master(serialPort);
-                    if (node.modbusMaster) {
+            log.info(vsprintf("About to create serial port on device %s (%s baud)", [node.serial_device, node.serial_speed]));
+
+            node.serialPort = new SerialPort(node.serial_device, {
+                baudrate: node.serial_speed});
+            if (node.serialPort) {
+                log.info("Created the serial port.");
+                new modbus.Master(node.serialPort, function (master) {
+                    if (master) {
+                        node.modbusMaster = master;
                         log.info("Created modbus master device");
                         callback(node.modbusMaster,null);
                     }
                     else {
                         callback(null, "Failed to create modbus master device");
                     }
+
+                });
+                // handle serial port open
+                node.serialPort.on('open', function() {
+                    log.info("Opened the serial port.");
                 });
                 // handle serial port opening failure
-                serialPort.on('error', function(err) {
+                node.serialPort.on('error', function(err) {
                     log.error('Error: ', err.message);
                 });
                 
             }
             else {
-                callback(null, "Failed to create serial port");
+                callback(null, "Failed to create a serial port");
             }
         };
     }
--- src/modbus-rtu-in.js
+++ src/modbus-rtu-in.js
@@ -6,15 +6,19 @@ module.exports = function(RED) {
     };
     Object.freeze(ModbusState);
     
-    function ModbusRTUInNode(config) {
-        var log = require('tracer').console();
+    function modbusRTUInNode(config) {
+        //var log = require('tracer').console();
+        var log = RED.log;
+        var vsprintf = require("sprintf-js").vsprintf;
+        var Q = require("q");
+        
         RED.nodes.createNode(this,config);
         var node = this;
+        node.name = config.name;
         node.modbusMaster = null;
         
-        log.info("Instantiated ModbusRTUInNode");
+        log.info("Instantiated ModbusRTUInNode '" + node.name + "'");
         var modbusConnection = RED.nodes.getNode(config.modbusConnection);
-        log.info("Found modbusConnection object");
         modbusConnection.initializeRTUConnection(
             function (modbusMaster, err) {
                 if (err) {
@@ -27,9 +31,26 @@ module.exports = function(RED) {
                 else {
                     log.error('Modbus connection failed, but no error reported.');
                 }
+            }
+        );
+
+        function readHoldingRegisters(slave, startRegister, nbrOfRegisters) {
+            log.info(vsprintf('readHolding registers called with: %s, %s, %s',[slave, startRegister, nbrOfRegisters]));
+            node.modbusMaster.readHoldingRegisters(slave, startRegister, nbrOfRegisters).
+            then(function(data){
+                node.send({'payload': data});
+            }, function(err){
+                log.error('Failure on holding register read: ', err);
             });
-        
+        }
+        node.on('input', function(msg) {
+            log.info('Input message:',msg);
+            readHoldingRegisters(msg.payload.slave,msg.payload.startRegister,msg.payload.nbrOfRegisters); 
+        });
+ 
+
     }
-    RED.nodes.registerType("modbus-rtu-in",ModbusRTUInNode);
+    
+    RED.nodes.registerType("modbus-rtu-in",modbusRTUInNode);
 
 };
